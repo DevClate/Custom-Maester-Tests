2f8afc7d092ed8089bb9ae2fd3bc9d7febef8d7c
Clayton <107187714+DevClate@users.noreply.github.com>
2025-01-15 07:36:25 -0500

    Add Docusaurus configuration for Custom Maester Test Collection

---
+++ website/docusaurus.config.ts
@@ -0,0 +1,142 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const config: Config = {
+  title: 'Custom Maester Test Collection',
+  tagline: 'Collection of custom tests for Maester',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://devclate.github.io/',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/Custom-Maester-Tests/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'DevClate', // Usually your GitHub org/user name.
+  projectName: 'Custom-Maester-Tests', // Usually your repo name.
+
+  trailingSlash: true, // Set to true if you are using GitHub pages
+
+  onBrokenLinks: 'warn',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: 'https://github.com/DevClate/Custom-Maester-Tests/edit/main/website/',
+        },
+        blog: {
+          showReadingTime: true,
+          feedOptions: {
+            type: ['rss', 'atom'],
+            xslt: true,
+          },
+
+          // Useful options to enforce blogging best practices
+          onInlineTags: 'warn',
+          onInlineAuthors: 'warn',
+          onUntruncatedBlogPosts: 'warn',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/docusaurus-social-card2.png',
+    navbar: {
+      title: 'Custom Maester Test Collection',
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/logo1r.svg',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Docs',
+        },
+        { to: '/docs/category/tests', label: 'Tests', position: 'left' },
+        { to: '/blog', label: 'Blog', position: 'left' },
+        {
+          href: 'https://github.com/DevClate/Maester-Tests',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Introduction',
+              to: '/docs/intro',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'BlueSky',
+              href: 'https://bsky.app/profile/devclate.bsky.social',
+            },
+            {
+              label: 'X',
+              href: 'https://x.com/clatent',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: '/blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/DevClate/Maester-Tests',
+            },
+            {
+              label: 'Maester',
+              href: 'https://maester.dev',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Custom Maester Test Collection, Inc. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
