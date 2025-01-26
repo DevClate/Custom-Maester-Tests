80cf96d6e288cbdcb8ad9940f93d5264c3a6005b
Clayton <107187714+DevClate@users.noreply.github.com>
2025-01-26 05:52:25 -0500

    Fix test case identifier for business number format validation in Contoso user tests

--- tests/Entra/Formatting/Test-ContosoUsersFormattingBusinessNumberUSA2.tests.ps1
+++ tests/Entra/Formatting/Test-ContosoUsersFormattingBusinessNumberUSA2.tests.ps1
@@ -7,7 +7,7 @@ BeforeAll {
 
 Describe "Contoso" -Tag "ENTRA.UF.1002.T02.USA", "Entra", "CustomTests", "USA", "Users", "BusinessNumber" {
 
-    It "ENTRA.UF.1003.USA: Business Number - All users should have a valid format" {
+    It "ENTRA.UF.1002.USA: Business Number - All users should have a valid format" {
         $result = Test-ContosoUsersFormattingBusinessNumberUSA2
         $result | Should -Be $true -Because "All users must have a valid business number in (123) 456-7890 format."
     }
